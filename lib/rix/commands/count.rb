d7045b085b817270253aa12a9278cc0e0777cab3
Richard Hubers <richard.hubers@gmail.com>
2011-08-21 16:22:39 +0200

    Added count command
    This is currently just an edited copy of show.

---
+++ lib/rix/commands/count.rb
@@ -0,0 +1,43 @@
+module Rix
+  module Commands
+    class Count < Command
+      attr_accessor :files
+      attr_accessor :xpath
+
+      def opts
+        OptionParser.new do |opts|
+          opts.banner = "Usage: rix count <xpath expression> [file(s) and/or pattern(s)]"
+        end
+      end
+
+      def help
+        opts.help
+      end
+
+      def execute
+        @files = []
+
+        opts.parse!
+
+        @xpath = ARGV.shift.dup if ARGV.size > 0
+        # @files will hold all remaining command-line arguments on Windows,
+        # and expanded filenames on Mac, Linux etc.
+        @files = ARGV.uniq      if ARGV.size > 1
+
+        raise "Missing XPath expression" if @xpath.nil? or @xpath.empty?
+
+        @files.each do |pattern|
+          # Using Dir is only needed for Windows, where arguments are not expanded,
+          # on Mac, Linux etc. this will have no effect.
+          Dir[pattern].each do |path|
+            File.open(path) do |file|
+              document = REXML::Document.new(file)
+              nodes = REXML::XPath.match(document, @xpath)
+              puts "#{path}: #{nodes.size}"
+            end
+          end
+        end
+      end
+    end
+  end
+end
