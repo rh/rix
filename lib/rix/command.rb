bc8a77eafcf88451bf1a57dc34da9d96959db512
Richard Hubers <richard.hubers@gmail.com>
2011-08-22 18:33:02 +0200

    Extracted common code into Command and rewrote Show

--- lib/rix/command.rb
+++ lib/rix/command.rb
@@ -2,5 +2,67 @@ require 'rix/commands/unknown'
 
 module Rix
   class Command
+    attr_accessor :files
+    attr_accessor :xpath
+
+    def opts
+      OptionParser.new
+    end
+
+    def help
+      opts.help
+    end
+
+    def execute
+      @files = []
+      @value = ""
+
+      opts.parse!
+
+      @xpath = ARGV.shift.dup if ARGV.size > 0
+      # @files will hold all remaining command-line arguments on Windows,
+      # and expanded filenames on Mac, Linux etc.
+      @files = ARGV.uniq      if ARGV.size > 0
+
+      raise "Missing XPath expression" if @xpath.nil? or @xpath.empty?
+
+      before_all
+
+      @files.each do |pattern|
+        # Using Dir is only needed for Windows, where arguments are not expanded,
+        # on Mac, Linux etc. this will have no effect.
+        Dir[pattern].each do |path|
+          File.open(path, 'r+') do |file|
+            document = REXML::Document.new(file)
+            nodes = REXML::XPath.match(document, @xpath)
+            before(path, nodes)
+            nodes.each do |node|
+              on_node(node)
+            end
+            after
+            #file.truncate 0
+            #file.pos = 0
+            #document.write file
+          end
+        end
+      end
+
+      after_all
+    end
+
+    def before_all
+    end
+
+    def before(path, nodes)
+    end
+
+    def on_node(node)
+    end
+
+    def after
+    end
+
+    def after_all
+    end
   end
 end
--- lib/rix/commands/show.rb
+++ lib/rix/commands/show.rb
@@ -1,49 +1,18 @@
 module Rix
   module Commands
     class Show < Command
-      attr_accessor :files
-      attr_accessor :xpath
-
-      def opts
-        OptionParser.new do |opts|
-          opts.banner = "Usage: rix show <xpath expression> [file(s) and/or pattern(s)]"
-        end
+      def initialize
+        @formatter = REXML::Formatters::Pretty.new
+        @formatter.compact = true
       end
 
       def help
-        opts.help
+        "Usage: rix show <xpath> <files>"
       end
 
-      def execute
-        @files = []
-
-        opts.parse!
-
-        @xpath = ARGV.shift.dup if ARGV.size > 0
-        # @files will hold all remaining command-line arguments on Windows,
-        # and expanded filenames on Mac, Linux etc.
-        @files = ARGV.uniq      if ARGV.size > 0
-
-        raise "Missing XPath expression" if @xpath.nil? or @xpath.empty?
-
-        formatter = REXML::Formatters::Pretty.new
-        formatter.compact = true
-
-        @files.each do |pattern|
-          # Using Dir is only needed for Windows, where arguments are not expanded,
-          # on Mac, Linux etc. this will have no effect.
-          Dir[pattern].each do |path|
-            File.open(path) do |file|
-              document = REXML::Document.new(file)
-              nodes = REXML::XPath.match(document, @xpath)
-              puts "#{path}: #{nodes.size}"
-              nodes.each do |node|
-                formatter.write(node, $stdout)
-                puts
-              end
-            end
-          end
-        end
+      def on_node(node)
+        @formatter.write(node, $stdout)
+        puts
       end
     end
   end
