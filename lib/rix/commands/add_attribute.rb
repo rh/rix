c8253fe5cd0188090438487325560938890c819f
Richard Hubers <richard.hubers@gmail.com>
2011-08-25 20:18:06 +0200

    Added add-attribute command

---
+++ lib/rix/commands/add_attribute.rb
@@ -0,0 +1,32 @@
+module Rix
+  module Commands
+    class AddAttribute < Command
+      attr_accessor :name
+      attr_accessor :value
+
+      def opts
+        OptionParser.new do |opts|
+          opts.banner = "Usage: rix add-attribute [options] <xpath> <files>"
+          opts.separator ""
+          opts.separator "Options:"
+          opts.on("-n", "--name NAME", "The name of the new attribute") { |name| @name = name }
+          opts.on("-v", "--value VALUE", "The value of the new attribute") { |value| @value = value }
+        end
+      end
+
+      def before_all
+        raise "Missing option: -n, --name" if @name.nil? or @name.empty?
+      end
+
+      def on_node(node)
+        node.add_attribute(@name, @value) if node.is_a? REXML::Element
+      end
+
+      def after(path, document)
+        File.open(path, 'w') do |file|
+          document.write file
+        end
+      end
+    end
+  end
+end
