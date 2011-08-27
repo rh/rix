9bba29a6a1959ae406cad4895fc2f474a7128c15
Richard Hubers <richard.hubers@gmail.com>
2011-08-27 22:31:05 +0200

    Added tests for AddAttribute

--- spec/rix/commands/add_attribute_spec.rb
+++ spec/rix/commands/add_attribute_spec.rb
@@ -3,6 +3,24 @@ require 'spec_helper'
 module Rix
   module Commands
     describe AddAttribute do
+      before(:all) do
+        @command = AddAttribute.new
+        @command.name = "foo"
+        @command.value = "bar"
+        @element = Element.new "element"
+      end
+
+      it "adds a new attribute" do
+        @element.attributes.get_attribute("foo").should == nil
+        @command.on_element(@element)
+        @element.attributes["foo"].should == "bar"
+      end
+
+      it "changes the value of an existing attribute" do
+        @element.add_attribute @command.name, "value"
+        @command.on_element(@element)
+        @element.attributes["foo"].should == "bar"
+      end
     end
   end
 end
