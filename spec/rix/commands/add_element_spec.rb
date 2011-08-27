208c325b79aac34c3e5b15f3e789dfb8a52ca401
Richard Hubers <richard.hubers@gmail.com>
2011-08-27 22:45:44 +0200

    Added tests for AddElement

--- spec/rix/commands/add_element_spec.rb
+++ spec/rix/commands/add_element_spec.rb
@@ -3,6 +3,14 @@ require 'spec_helper'
 module Rix
   module Commands
     describe AddElement do
+      it "adds a new element" do
+        command = AddElement.new
+        command.name = "foo"
+        command.value = "bar"
+        element = Element.new
+        command.on_element(element)
+        element.elements[command.name].text.should == "bar"
+      end
     end
   end
 end
