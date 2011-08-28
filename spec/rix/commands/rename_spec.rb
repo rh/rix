326893d865b6271cddbb774ac4315772c2ee3f46
Richard Hubers <richard.hubers@gmail.com>
2011-08-28 19:39:25 +0200

    Added tests for Rename

--- spec/rix/commands/rename_spec.rb
+++ spec/rix/commands/rename_spec.rb
@@ -3,6 +3,23 @@ require 'spec_helper'
 module Rix
   module Commands
     describe Rename do
+      it "renames elements" do
+        command = Rename.new
+        command.name = "after"
+        element = Element.new "before"
+        element.name.should == "before"
+        command.on_element(element)
+        element.name.should == command.name
+      end
+
+      it "renames attributes" do
+        command = Rename.new
+        command.name = "after"
+        attribute = Attribute.new "before"
+        attribute.name.should == "before"
+        command.on_attribute(attribute)
+        attribute.name.should == command.name
+      end
     end
   end
 end
