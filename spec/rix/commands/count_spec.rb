985b498460ce921681f8228b8c472e3c64bf58a1
Richard Hubers <richard.hubers@gmail.com>
2011-08-28 19:16:46 +0200

    Added tests for Count

--- spec/rix/commands/count_spec.rb
+++ spec/rix/commands/count_spec.rb
@@ -3,6 +3,21 @@ require 'spec_helper'
 module Rix
   module Commands
     describe Count do
+      before(:all) do
+        @out = StringIO.new
+        command = Count.new
+        command.out = @out
+        # String has a size method, so we use that for the 'nodes' parameter
+        command.before("name", nil, "123")
+      end
+
+      it "writes the name to the output" do
+        @out.string.should match(/^name/)
+      end
+
+      it "writes the number of selected nodes to the output" do
+        @out.string.should match(/3$/)
+      end
     end
   end
 end
