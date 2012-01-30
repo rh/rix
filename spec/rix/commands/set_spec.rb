afbee6ff363eebb77e35f33f4c34c58236909360
Richard Hubers <richard.hubers@gmail.com>
2012-01-30 22:07:54 +0100

    Refactored tests

--- spec/rix/commands/set_spec.rb
+++ spec/rix/commands/set_spec.rb
@@ -5,55 +5,52 @@ module Rix
     describe Set do
       before(:each) do
         @command = Set.new
-        @command.value = "after"
         @element = Element.new "element"
         @element.text = "before"
         @attribute = Attribute.new("attribute", "before")
         @element.add_attribute @attribute
       end
 
-      it "sets the value of an element" do
-        @command.on_element(@element)
-        @element.text.should == "after"
+      context "with #value set" do
+        before(:each) do
+          @command.value = "after"
+        end
+
+        it "sets the value of an element" do
+          @command.on_element(@element)
+          @element.text.should == "after"
+        end
+
+        it "sets the value of an attribute" do
+          result = @command.on_attribute(@attribute)
+          result.to_s.should == "after"
+        end
+
+        it "sets the value of text nodes" do
+          text = Text.new("before")
+          @element.add text
+          result =  @command.on_text text
+          result.should == "after"
+        end
       end
 
-      it "sets the value of an attribute" do
-        result = @command.on_attribute(@attribute)
-        result.to_s.should == "after"
-      end
+      context "with #value not set" do
+        it "clears the value of an element" do
+          @command.on_element(@element)
+          @element.text.should == ""
+        end
 
-      it "sets the value of text nodes" do
-        text = Text.new("before")
-        @element.add text
-        result =  @command.on_text text
-        result.should == "after"
-      end
+        it "clears the value of an attribute" do
+          result = @command.on_attribute(@attribute)
+          result.to_s.should == ""
+        end
 
-      it "clears the value of an element" do
-        command = Set.new
-        element = Element.new "element"
-        command.on_element(element)
-        element.text.should == ""
-      end
-
-      it "clears the value of an attribute" do
-        command = Set.new
-        element = Element.new "element"
-        attribute = Attribute.new("attribute", "before")
-        element.add_attribute attribute
-        result = command.on_attribute(attribute)
-        result.to_s.should == ""
-      end
-
-      it "clears the value of text nodes" do
-        command = Set.new
-        element = Element.new "element"
-        attribute = Attribute.new("attribute", "before")
-        element.add_attribute attribute
-        text = Text.new("before")
-        element.add text
-        result = command.on_text(text)
-        result.should == ""
+        it "clears the value of text nodes" do
+          text = Text.new("before")
+          @element.add text
+          result = @command.on_text(text)
+          result.should == ""
+        end
       end
     end
   end
